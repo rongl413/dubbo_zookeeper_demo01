2d89fe82c68601eeaf4f21615682e81388ab9b5b
rongl413 <1021047231@qq.com>
2020-04-16 10:39:13 +0800

    组员c修改test01

--- zd_consumer/src/main/java/com/java/consumer/controller/Test01.java
+++ zd_consumer/src/main/java/com/java/consumer/controller/Test01.java
@@ -5,4 +5,7 @@ package com.java.consumer.controller;
  * time:2020/4/16  10:16
  */
 public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("demo1");
+    }
 }
